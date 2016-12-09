c1c66fdf4960c9bce46c8839092c6e41975b0507
Jonathan Heard <jonathan.heard@walmart.com>
2016-12-08 19:54:27 -0600

    New command

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'It\'s nice to talk to you directly.')
 })
 
+controller.hears(['xur'], ['direct_message'], function (bot, message) {
+  bot.reply(message, 'I\'m not him, yet.')
+  bot.reply(message, 'It\'s nice to talk to you directly.')
+})
+
 controller.hears('.*', ['mention'], function (bot, message) {
   bot.reply(message, 'You really do care about me. :heart:')
 })
